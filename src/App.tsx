4aad7a431dc76fd6c02311b1cae11c021fdf1a99
EBU ENES YILDIRIM <86044516+ebuenesy2@users.noreply.github.com>
2023-02-14 03:07:42 +0300

    react

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,6 @@ import Login from './pages/login';
 import Users from './pages/users';
 import Clients from './pages/clients';
 import Question from './pages/question';
-import Message from './pages/message';
-import Gallery from './pages/gallery';
-import Logs from './pages/logs';
-
 import Setting from './pages/setting';
 
 
@@ -30,10 +26,6 @@ import Setting from './pages/setting';
           <Route path="/users" element={<Users/>} />
           <Route path="/clients" element={<Clients/>} />
           <Route path="/question" element={<Question/>} />
-          <Route path="/message" element={<Message/>} />
-          <Route path="/gallery" element={<Gallery/>} />
-          <Route path="/logs" element={<Logs/>} />
-         
           <Route path="/setting" element={<Setting/>} />
          
        </Routes>
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -4,32 +4,15 @@ import WidgetsIcon from '@material-ui/icons/Widgets';
 import ColorLensIcon from '@material-ui/icons/ColorLens';
 import PersonIcon from '@material-ui/icons/Person';
 import PeopleIcon from '@material-ui/icons/People';
-import StoreIcon from '@material-ui/icons/Store';
-import PaymentIcon from '@material-ui/icons/Payment';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import SettingsSystemDaydreamIcon from '@material-ui/icons/SettingsSystemDaydream';
-import SettingsBrightnessIcon from '@material-ui/icons/SettingsBrightness';
-import SettingsIcon from '@material-ui/icons/Settings';
-import ListIcon from '@material-ui/icons/List';
-import NoteIcon from '@material-ui/icons/Note';
-import PermMediaIcon from '@material-ui/icons/PermMedia';
-import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
-import ContactPhoneOutlinedIcon from '@material-ui/icons/ContactPhoneOutlined';
-import CalendarTodayOutlinedIcon from '@material-ui/icons/CalendarTodayOutlined';
-import PersonOutlinedIcon from '@material-ui/icons/PersonOutlined';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import VideocamIcon from '@material-ui/icons/Videocam';
-import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
-import LiveHelpIcon from '@material-ui/icons/LiveHelp';
+
 
 function Index() {
     
   return (
         <div className='sidebar'>
              <div className="top">
-                <span className="logo"> SocialEducation </span>
+                <span className="logo"> Anket </span>
              </div>
              <hr />
              <div className="center">
@@ -82,119 +65,7 @@ function Index() {
                         <span> Question </span>
                      </a>
                    </li>
-                   <li>
-                     <a href="/#">
-                        <MenuBookIcon className="icon" />
-                        <span> Trial Exams </span>
-                     </a>
-                   </li>
-                   <p className="title">  Lesson </p>
-                   <li>
-                     <a href="/#">
-                        <VideocamIcon className="icon" />
-                        <span> Lesson Video </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a href="/#">
-                        <InsertDriveFileIcon className="icon" />
-                        <span> Lesson Notes </span>
-                     </a>
-                   </li>
-
-                   <p className="title">  USEFUL</p>
-                   <li>
-                     <a href="/message">
-                        <MailOutlineIcon className="icon" />
-                        <span> Messages </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a href="/#">
-                        <NotificationsIcon className="icon" />
-                        <span> Notifications </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a href="/#">
-                        <NoteIcon className="icon" />
-                        <span> Notes </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a href="/#">
-                        <ListIcon className="icon" />
-                        <span> Todo List </span>
-                     </a>
-                   </li>
-
-                   <p className="title">  SHOPS </p>
-                   <li>
-                     <a href="/#">
-                        <StoreIcon className="icon" />
-                        <span> Products </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a href="/#">
-                        <PaymentIcon className="icon" />
-                        <span> Orders </span>
-                     </a>
-                   </li>
-
-                   <p className="title">  App </p>
-                   <li>
-                     <a href="/gallery">
-                        <PermMediaIcon className="icon" />
-                        <span> Gallery </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a href="/#">
-                        <FileCopyOutlinedIcon className="icon" />
-                        <span> FileManager </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a href="/#">
-                        <ContactPhoneOutlinedIcon className="icon" />
-                        <span> Contact </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a href="/#">
-                        <CalendarTodayOutlinedIcon className="icon" />
-                        <span> Calendar </span>
-                     </a>
-                   </li>
-
-                   <p className="title">  SERVICE</p>
-                   <li>
-                     <a href="/#">
-                        <LiveHelpIcon className="icon" />
-                        <span> Faq </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a href="/#">
-                        <SettingsSystemDaydreamIcon className="icon" />
-                        <span> System Health </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a href="/logs">
-                        <SettingsBrightnessIcon className="icon" />
-                        <span> Logs </span>
-                     </a>                     
-                   </li>
                  
-                   <li>
-                     <a href="/setting">
-                        <SettingsIcon className="icon" />
-                        <span> Settings </span>
-                     </a>
-                   </li>
-                  
 
                 </ul>
              </div>
