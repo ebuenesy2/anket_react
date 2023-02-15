07252ddd0959b806f32002eba3435142d3e08e95
ebuenesy2 <ebuenesy2@gmail.com>
2023-02-15 10:23:12 +0300

    question

--- src/pages/question/index.tsx
+++ src/pages/question/index.tsx
@@ -1,8 +1,7 @@
 import "./index.css"
 import Navbar from "../../components/navbar";
 import Sidebar from "../../components/sidebar";
-import CircularProgressbar from '../../components/Items/CircularProgressbar';
-import Progressbar from '../../components/Items/Progressbar';
+
 
 //! item
 import FormControlLabel from "@material-ui/core/FormControlLabel";
@@ -15,10 +14,8 @@ import TouchAppIcon from '@material-ui/icons/TouchApp';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
-function onChangeValue(event:any) { 
-     console.log("onChangeValue:",event.target.value);
-}
 
 function Index() {
     
@@ -28,9 +25,17 @@ function Index() {
        const [success, setSuccess] = useState(0);
        const [answersData, setAnswersData] = useState<any[]>([])
        const [questionTitle, setQuestionTitle] = useState("");
+       const [answerChange, setanswerChange] = useState(null);
        
      
+       //! Radio
+       function onChangeValue(id:any) { 
+           //console.log("id:",id);
+           setanswerChange(id);
+       };
      
+     
+       //! Data from API
        useEffect(() => { apiGet(); }, []);
 
        const apiGet = () => {  
@@ -42,15 +47,15 @@ function Index() {
 
           setSearchId(Number(searchIdValue)); //! Search
 
-          console.log("searchIdValue:",searchIdValue);
+          //console.log("searchIdValue:",searchIdValue);
 
          const apiUrl_table=process.env.REACT_APP_API_URL+"/api/survey/"+searchIdValue;
-         console.log("apiUrl_table:",apiUrl_table);
+         //console.log("apiUrl_table:",apiUrl_table);
          
          axios.get(apiUrl_table)
               .then(response => {
                 
-                   console.log("response:", response.data);
+                   //console.log("response:", response.data);
      
                
                    setSuccess(response.data.status); //! Başarılı
@@ -58,12 +63,94 @@ function Index() {
                    setQuestionTitle(response.data.DB.question); //! Soru
                    setAnswersData(response.data.DB.answers); //! Cevaplar
                    
-                   console.log("answersData:", answersData[0].title);
+                   //console.log("answersData:", answersData[0].title);
                
            })
            .catch(error => {  console.log("Api Error:",error.message); });
          
-       };
+       };   //! Data from API
+
+
+       //! Voting function
+       const postSurvey = () => {
+         console.log("answerChange:",answerChange);
+
+          if(answerChange == null) {
+
+               Swal.fire({
+                    position: 'center',
+                    icon: 'error',
+                    title: 'Seçim Yapılmadı',
+                    showConfirmButton: false,
+                    timer: 2000
+                  });
+          }
+          else {
+
+               const apiUrl_table=process.env.REACT_APP_API_URL+"/api/survey_vote/add";
+               console.log("apiUrl_table:",apiUrl_table);
+
+               //! Data to be posted
+               const NewData = {
+                    serverId:0,
+                    serverToken:"yildirimdev",
+                    voterId: Number(localStorage.getItem('userId')) ,
+                    surveyId:searchId,
+                    surveyAnswersId: Number(answerChange),
+                    created_byToken: localStorage.getItem('userToken')
+               }; //! Data to be posted
+
+
+               console.log("NewData:",NewData);
+
+               
+               axios.post(apiUrl_table,NewData)
+               .then(response => {
+                    
+                    //! State
+                    setSuccess(response.data.status); //! Durum
+                    
+                    //! console
+                    console.log("Data:", response.data);
+                    
+                    
+                    if (response.data.status == 1) {
+                    
+                         Swal.fire({
+                              position: 'center',
+                              icon: 'success',
+                              title: 'İşleminiz Başarılı',
+                              showConfirmButton: false,
+                              timer: 2000
+                         });
+
+                      
+                          //window.location.href ="/survey";
+                    
+                    }
+                    
+                    else if (response.data.status == 0) {
+                    
+                         Swal.fire({
+                              position: 'center',
+                              icon: 'error',
+                              title: 'İşleminiz Hatalı',
+                              showConfirmButton: false,
+                              timer: 2000
+                         });
+                    
+                    
+                    }
+                    
+                    
+               })
+               .catch(error => {  console.log("Api Error:",error.message); });
+
+
+
+          }
+
+       }; //! Voting function
      
   return (
         <div className='question'>
@@ -91,32 +178,32 @@ function Index() {
                                              <div className="answerBox">
                                                   <RadioGroup aria-labelledby="demo-radio-buttons-group-label" name="radio-buttons-group" >
                                                     <div className="answerDescription"  > 
-                                                       <div className="choiceBox"> 
+                                                       <div className="choiceBox" style={{ display:answersData[0]?.title == null ? "none" : "flex" }}> 
                                                             <div className="choice"> <p className="letter"> A </p> </div>
-                                                            <div className="choiceAnswer"> <FormControlLabel onChange={onChangeValue} value="a" control={<Radio />} label={answersData[0]?.title} /> </div>
+                                                            <div className="choiceAnswer"> <FormControlLabel onChange={() => {onChangeValue(answersData[0]?.id)}} value="a" control={<Radio />} label={answersData[0]?.title} /> </div>
                                                        </div>
 
-                                                       <div className="choiceBox"> 
+                                                       <div className="choiceBox" style={{ display:answersData[1]?.title == null ? "none" : "flex" }}> 
                                                             <div className="choice"> <p className="letter"> B </p> </div>
-                                                            <div className="choiceAnswer"> <FormControlLabel value="b" onChange={onChangeValue} control={<Radio />} label={answersData[1]?.title} /> </div>
+                                                            <div className="choiceAnswer"> <FormControlLabel onChange={() => {onChangeValue(answersData[1]?.id)}} value="b" control={<Radio />} label={answersData[1]?.title} /> </div>
                                                        </div>
 
                                                        <div className="choiceBox" style={{ display:answersData[2]?.title == null ? "none" : "flex" }} > 
                                                             <div className="choice"> <p className="letter"> C </p> </div>
-                                                            <div className="choiceAnswer"> <FormControlLabel value="c" onChange={onChangeValue} control={<Radio />} label={answersData[2]?.title} /> </div>
+                                                            <div className="choiceAnswer"> <FormControlLabel onChange={() => {onChangeValue(answersData[2]?.id)}} value="c" control={<Radio />} label={answersData[2]?.title} /> </div>
                                                        </div>
 
-                                                       <div className="choiceBox" style={{ display:answersData[2]?.title == null ? "none" : "flex" }} > 
+                                                       <div className="choiceBox" style={{ display:answersData[3]?.title == null ? "none" : "flex" }} > 
                                                             <div className="choice"> <p className="letter"> D </p> </div>
-                                                            <div className="choiceAnswer"> <FormControlLabel value="d" onChange={onChangeValue} control={<Radio />} label={answersData[3]?.title} /> </div>
+                                                            <div className="choiceAnswer"> <FormControlLabel onChange={() => {onChangeValue(answersData[3]?.id)}} value="d" control={<Radio />} label={answersData[3]?.title} /> </div>
                                                        </div>
                                                   </div>
                                                   </RadioGroup>
 
                                                   <div className="buttonBox">
-                                                       <div className="buttonPre"> <ArrowLeftIcon className="icon" /> </div>
-                                                       <div className="buttonPre"> <TouchAppIcon className="icon" /> </div>
-                                                       <div className="buttonPre"> <ArrowRightIcon className="icon" /> </div>
+                                                       <div className="buttonPre"> <ArrowLeftIcon className="icon" onClick={() => { window.location.href="/survey"; }} /> </div>
+                                                       <div className="buttonPre"> <TouchAppIcon className="icon" onClick={() => {postSurvey(); }} /> </div>
+                                                       <div className="buttonPre"> <ArrowRightIcon className="icon" onClick={() => { window.location.href="/survey"; } } /> </div>
                                                   </div>
                                              </div>
                                         </div>
